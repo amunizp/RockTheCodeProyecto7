166847de26d65a9b09312ac490b0eca594596c6f
Andres Muniz Piniella <amp-git@c4ad.eu>
2024-09-16 13:00:29 +0100

    seed fixed

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,37 +1,37 @@
 const locations = [
   {
-    _id: { $oid: '66e3fffd4fd7f2c6c6ee6903' },
+    _id: '66e3fffd4fd7f2c6c6ee6903',
     geo: { coordinates: [] },
     court: 'Brooke',
-    courtNumber: { $numberInt: '8' },
+    courtNumber: 8,
     issues: [],
-    createdAt: { $date: { $numberLong: '1726218237514' } },
-    updatedAt: { $date: { $numberLong: '1726218237514' } },
-    __v: { $numberInt: '0' }
+    createdAt: '1726218237514',
+    updatedAt: '1726218237514',
+    __v: 0
   },
   {
-    _id: { $oid: '66e2ef5507d90c40cbcc7e48' },
+    _id: '66e2ef5507d90c40cbcc7e48',
     geo: { coordinates: [] },
     court: 'Spencer',
-    courtNumber: { $numberInt: '5' },
+    courtNumber: 5,
     issues: [
-      { $oid: '66e40481ba6df7172632c860' },
-      { $oid: '66e40493ba6df7172632c862' },
-      { $oid: '66e4049cba6df7172632c864' }
+      '66e40481ba6df7172632c860',
+      '66e40493ba6df7172632c862',
+      '66e4049cba6df7172632c864'
     ],
-    createdAt: { $date: { $numberLong: '1726148437063' } },
-    updatedAt: { $date: { $numberLong: '1726221765201' } },
-    __v: { $numberInt: '0' }
+    createdAt: '1726148437063',
+    updatedAt: '1726221765201',
+    __v: 0
   },
   {
-    _id: { $oid: '66e814006a8ab4da660dcc1e' },
+    _id: '66e814006a8ab4da660dcc1e',
     geo: { coordinates: [] },
     court: 'Pope',
-    courtNumber: { $numberInt: '9' },
+    courtNumber: 9,
     issues: [],
-    createdAt: { $date: { $numberLong: '1726485504354' } },
-    updatedAt: { $date: { $numberLong: '1726485504354' } },
-    __v: { $numberInt: '0' }
+    createdAt: '1726485504354',
+    updatedAt: '1726485504354',
+    __v: 0
   }
 ]
 
--- src/utils/locations.seed.js
+++ src/utils/locations.seed.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { default: mongoose } = require('mongoose')
 const { Location } = require('../api/models/location')
 const { locations } = require('../data/locations')
@@ -5,18 +6,25 @@ const { locations } = require('../data/locations')
 mongoose
   .connect(process.env.DB_URL)
   .then(async () => {
-    const allLocations = await Location.findOne() //solo tengo que encontrar una para borrar
-
-    if (allLocations.length) {
+    const anyLocations = await Location.findOne() //solo tengo que encontrar una para borrar
+    console.log(anyLocations)
+    if (anyLocations) {
       await Location.collection.drop()
     } else {
       console.log('nothing to delete')
     }
   })
-  .catch((err) => console.log(`Something went wrong delteing data: ${err}`))
+  .catch((err) => {
+    console.log(`Something went wrong delteing data: ${err}`)
+    //this stays hanging in case of an error. I wish for it to just disconnect.
+    //process.exit(1) //source: https://github.com/Automattic/mongoose/issues/4135
+    //mongoose.disconnect // this does not seem to work.
+  })
   .then(async () => {
     await Location.insertMany(locations)
-    console.log('I insterted many')
+    console.log('I inserted many')
   })
-  .catch((err) => console.log(`Error creating data: ${err}`))
+  .catch((err) => {
+    console.log(`Error creating data: ${err}`)
+  })
   .finally(() => mongoose.disconnect)
